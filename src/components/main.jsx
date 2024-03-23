bf9681eb664e27ec86f0497c0e15082f6fb3f522
Felipe gabriel <felipedeev@gmail.com>
2024-03-23 18:22:56 -0300

    Remoção da biblioteca 'ScrollReveal'

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,6 @@
-import ScrollReveal from 'scrollreveal'
 import styles from './styles/main.module.css'
 
 export default function MainJSX(){
-    ScrollReveal({ reset: true });
-    ScrollReveal().reveal("#main", {duration: 2000, scale: 2})
-
     return(
         <main id='main'>
             <h1> <span className={styles.Highlight}>[</span> Explore, Transacione, Divirta-se! <span className={styles.Highlight}>]</span> </h1>
