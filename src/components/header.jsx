f270b305e620f548fda266eb46c88b95c60e8a5e
Felipe gabriel <felipedeev@gmail.com>
2024-03-24 15:22:00 -0300

    Adição do scrollreveal, adição de botões e alteração nos textos das tag 'a'

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,14 +13,14 @@ export default function HeaderJSX(){
     return(
         <header>
             <div className={styles.titlebox}>
-                <h1><a href="/">Hypeon</a></h1>
+                <h1><a href="/">Hypecoin</a></h1>
             </div>
             <nav className={`${styles.nav} ${toggleClassCheck}`}>
                 <button onClick={handleClass} className={styles.hamburguer}></button>
                 <ul>
-                    <li><a href="#">Contas</a></li>
-                    <li><a href="#">Itens</a></li>
-                    <li><a href="#">Suporte</a></li>
+                    <li><a href="#">Explore</a></li>
+                    <li><a href="#">Login</a></li>
+                    <li className={styles.register}><a href="#">Registre-se</a></li>
                 </ul>
             </nav>
         </header>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import styles from './styles/main.module.css'
-
+import ScrollReveal from 'scrollreveal'
 export default function MainJSX(){
+    ScrollReveal().reveal("#main", {distance : "100px", duration : 2300})
     return(
         <main id='main'>
             <h1> <span className={styles.Highlight}>[</span> Explore, Transacione, Divirta-se! <span className={styles.Highlight}>]</span> </h1>
             <h3> Contas, Jogos, Gift Cards e Mais em um só lugar</h3>
+            <a href="register">Começar no hypeon!</a>
         </main>
     )
 }
